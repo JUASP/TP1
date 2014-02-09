0735297a515676af61f924b1960847810186bc3f
JUASP <nikemare@hotmail.com>
2014-02-09 17:59:13 -0500

    Regle gone

--- Travail pratique 1/Regle.cpp
+++ Travail pratique 1/Regle.cpp
@@ -1,17 +1,96 @@
 /**
- * \file Regle.cpp
- * \brief Ce fichier contient une implantation des méthodes de la classe Regle
- * \author ?
- * \version 0.1
- * \date février 2014
- *
- */
+* \file Regle.cpp
+* \brief Ce fichier contient une implantation des mï¿½thodes de la classe Regle
+* \author ?
+* \version 0.1
+* \date fï¿½vrier 2014
+*
+*/
 
 #include "Regle.h"
 
 namespace tp1
 {
 
-	//Mettez l'implémentation de vos méthodes ici.
+#include "Regle.h"
+
+namespace tp1
+{
+
+/**
+* \fn Regle:: Regle()
+*/
+
+Regle::Regle()
+{
+   premisses = 0;
+   conclusions = 0;
+   utilisee = false;
+}
+
+/**
+* \fn Regle:: Regle(const Regle&)
+*
+* \param[in] V Une regle
+*/
+Regle::Regle(const Regle& source)
+{
+   premisses = source.premisses;
+   conclusions = source.conclusions;
+   utilisee = source.utilisee;
+
+}
+
+/**
+* \fn Regle:: ~Regle()
+*/
+Regle::~Regle()
+{
+   premisses = 0;
+   conclusions = 0;
+   utilisee = false;
+
+}
+
+/**
+* \fn Regle:: Regle(const Regle&)
+*
+* \param[in] V Une regle
+*/
+Regle& Regle::operator =(const Regle& source)
+{
+   return (*this);
+}
+
+/**
+* \fn Regle:: GetPremisses()
+*
+* \param[out] V Un pointeur vers premisses
+*/
+EnsembleFaits * Regle::GetPremisses()
+{
+return *premisses;
+}
+
+/**
+* \fn Regle:: GetPremisses()
+*
+* \param[out] V Un pointeur vers premisses
+*/
+EnsembleFaits * Regle::GetConclusions()
+{
+return *conclusions;
+}
+
+/**
+* \fn Regle:: GetPremisses()
+*
+* \param[out] V Un pointeur vers premisses
+*/
+bool Regle::GetUtilisee() const
+{
+   return *utilisee;
+}
+}
 
 }
